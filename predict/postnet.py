129f0e3be1f3a3f27476d25e5dc3850dbae62299
technillogue <technillogue@gmail.com>
2022-05-06 17:11:20 -0400

    updates

--- predict/postnet.py
+++ predict/postnet.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import dataclasses
 import random
 import torch
@@ -5,7 +6,17 @@ import tqdm
 from torch import Tensor, nn
 from get_embeddings import Prompt
 
+from torch.utils.tensorboard import SummaryWriter
+
+
 device = "cpu"
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
+
 
 net = nn.Sequential(
     nn.Linear(512, 512),
@@ -17,36 +28,42 @@ net = nn.Sequential(
     nn.Linear(512, 256),
     nn.ReLU(),
     nn.Linear(256, 1),
-    nn.Dropout(p=0.1),
+    nn.Dropout(p=0.2),
 ).to(device)
 
+net.apply(init_weights)
 
-def train() -> None:
-    prompts = torch.load("prompts.pth")  # type: ignore # 13k prompts
+
+def train(prompts: list[Prompt]) -> nn.Module:
     opt = torch.optim.Adam(net.parameters(), lr=1e-4)
     loss_fn = nn.L1Loss()
-    progress_bar = tqdm.tqdm(enumerate(prompts[: int(len(prompts) * 0.8)]))
+    progress_bar = tqdm.tqdm(enumerate(prompts))
+
+    writer = SummaryWriter()
+    current_loss_batch = []
     for i, prompt in progress_bar:
         opt.zero_grad()
         prediction = net(prompt.embed)
         actual = Tensor([[[float(bool(prompt.reacts))]]])
         loss = loss_fn(prediction, actual)
+        current_loss_batch.append(float(loss))
         loss.backward()
         opt.step()
-        if i % int(len(prompts) / 10) == 0:
-            progress_bar.set_description(f"i {i} loss: {loss}")
-
+        if (i + 1) % int(len(prompts) / 10) == 0:
+            avg_loss = sum(current_loss_batch) / len(current_loss_batch)
+            writer.add_scalar("Loss/train", avg_loss, i)
+            print(f"i {i} loss: {avg_loss}")
+    writer.flush()
     torch.save(net, "reaction_predictor.pth")
+    return net
 
 
-def validate() -> None:
-    net = torch.load("reaction_predictor.pth")  # type: ignore
-    prompts = torch.load("prompts.pth")  # type: ignore
-    test = list(prompts[-int(len(prompts) * 0.8) :])
-    random.shuffle(test)
+def validate(prompts: list[Prompt], net: Optional[nn.Module] = None) -> None:
+    if not net:
+        net = torch.load("reaction_predictor.pth")  # type: ignore
     loss_fn = nn.L1Loss()
     losses = []
-    for i, prompt in enumerate(test):
+    for i, prompt in enumerate(prompts):
         prediction = net(prompt.embed)
         actual = Tensor([[[float(bool(prompt.reacts))]]])
         if i < 20:
@@ -55,8 +72,7 @@ def validate() -> None:
             )
         loss = loss_fn(prediction, actual)
         losses.append(float(loss))
-    print(f"MSE: {round(sum(losses) / len(losses), 4)}")
-
+    print(f"L1: {round(sum(losses) / len(losses), 4)}")
 
 
 # MSE: 0.49840676848697946
@@ -68,5 +84,19 @@ def validate() -> None:
 # MSE: 0.2626
 # switch to 512-512-512-256-1
 # MSE: 0.2606
-train()
-validate()
+# xavier init, L1
+# 0.42
+# use batch loss in indicators, tweaks
+# 0.44
+if __name__ == "__main__":
+    prompts = torch.load("prompts.pth")  # type: ignore
+    train_set = []
+    valid_set = []
+    for i, prompt in enumerate(prompts):
+        if i % 10 < 8:
+            train_set.append(prompt)
+        else:
+            valid_set.append(prompt)
+    print(len(train_set), len(valid_set))
+    net = train(list(train_set))
+    validate(list(valid_set), net)
--- predict/server.py
+++ predict/server.py
@@ -1,9 +1,23 @@
 import torch
-from get_embeddings import embed
+from torch.nn import functional as F
 from aiohttp import web
 
-net = torch.load("reaction_predictor.pth")  # type: ignore
+from clip import clip
+
+net = torch.load("reaction_predictor")  # type: ignore
 app = web.Application()
+
+percep = clip.load("ViT-B/32", jit=False)[0]
+percep.eval()
+
+
+def norm(embedding: torch.Tensor) -> torch.Tensor:
+    return F.normalize(embedding.unsqueeze(1), dim=2)
+
+
+def embed(text: str) -> torch.Tensor:
+    "normalized clip embedding of text"
+    return norm(percep.encode_text(clip.tokenize(text, truncate=True)))
 
 
 async def handle_request(request: web.Request) -> web.Response:
