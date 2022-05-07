0252b3b7cbbb51916dad8cb8a0332d425b32f441
technillogue <technillogue@gmail.com>
2022-05-07 16:06:34 -0400

    round scores

--- predict/server.py
+++ predict/server.py
@@ -21,14 +21,14 @@ async def handle_request(request: web.Request) -> web.Response:
     embedding = embed(perceptor, text)
     tensor = net(embedding)
     logging.info(tensor)
-    score = float(tensor)
+    score = round(float(tensor), 4)
     logging.info(score)
     return web.Response(status=200, text=str(score))
 
 async def handle_info(request: web.Request) -> web.Response:
     text = await request.text()
     embedding = embed(perceptor, text)
-    score = float(net(embedding))
+    score = round(float(net(embedding)), 4)
     closest = knn(embedding, prompts, k = 1)[0]
     return web.json_response({"score": str(score), "closest": closest.prompt, "reactions": closest.reacts})
     Response(status=200, text=str(score))
